e141dd9ee06bfd915b67c1f7ed7f6f9888f0c00b
AShraf Mohamed <124946844+ashrafmo-1@users.noreply.github.com>
2024-02-07 16:17:06 +0200

    add repos length

--- js/master.js
+++ js/master.js
@@ -15,39 +15,40 @@ const showResposResult = () => {
     } else {
         fetch(`https://api.github.com/users/${inputSearch.value}/repos`).then((result) => result.json())
         .then((repository) => {
-            console.log(repository); // <===== print repository in console
+            console.log(repository.length); // <===== print repository in console
             repositorycontent.innerHTML = ``; // <===== impty repository content to run new repository
             // add all repositories to boxes-repos <=====
             repository.forEach((repos) => {
+                document.querySelector('.repo_length').innerHTML = `you hava ${repository.length} repositories`;
                 // master div <=====
                 const box = document.createElement('div');
-                box.className = 'box'
-                document.querySelector('.boxes-repos').appendChild(box)
+                box.className = 'box';
+                document.querySelector('.boxes-repos').appendChild(box);
                 // repository name <=====
                 const nameRepository = document.createElement('h1');
-                nameRepository.className = 'repo-name'
+                nameRepository.className = 'repo-name';
                 const nameRepositoryText = document.createTextNode(repos.name);
                 nameRepository.appendChild(nameRepositoryText);
                 // make box control repositories <=====
                 const controlsRepository = document.createElement('div');
-                controlsRepository.className = 'control'
+                controlsRepository.className = 'control';
                 //  link to the repository <=====
-                const visetRepo = document.createElement('a')
-                visetRepo.className = 'view-repo'
-                const textLink = document.createTextNode('viset repository')
+                const visetRepo = document.createElement('a');
+                visetRepo.className = 'view-repo';
+                const textLink = document.createTextNode('viset repository');
                 visetRepo.appendChild(textLink)
-                visetRepo.href = `https://github.com/${inputSearch.value}/${repos.name}` // repository link
-                visetRepo.setAttribute('target', '_blank')
-                controlsRepository.appendChild(visetRepo)
+                visetRepo.href = `https://github.com/${inputSearch.value}/${repos.name}`; // repository link
+                visetRepo.setAttribute('target', '_blank');
+                controlsRepository.appendChild(visetRepo);
                 // get starts length<=====
-                const starsSpan = document.createElement('span')
-                starsSpan.className = 'stars-lenght'
-                const textStars = document.createTextNode(`starts ${repos.stargazers_count}`)
-                starsSpan.appendChild(textStars)
-                controlsRepository.appendChild(starsSpan)
+                const starsSpan = document.createElement('span');
+                starsSpan.className = 'stars-lenght';
+                const textStars = document.createTextNode(`starts ${repos.stargazers_count}`);
+                starsSpan.appendChild(textStars);
+                controlsRepository.appendChild(starsSpan);
                 // append all to master div<=====
-                box.appendChild(nameRepository)
-                box.appendChild(controlsRepository)
+                box.appendChild(nameRepository);
+                box.appendChild(controlsRepository);
             })
         });
     }
